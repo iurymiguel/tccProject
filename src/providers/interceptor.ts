bba0786518d1d6c1f0613ea06e3ad4cb450ba3ff
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-07-09 20:29:36 -0300

    Pegando respostas de erros de requisição no interceptor

--- src/providers/interceptor.ts
+++ src/providers/interceptor.ts
@@ -5,17 +5,20 @@ import { Config } from "../config/config";
 
 @Injectable()
 export class HttpsRequestInterceptor implements HttpInterceptor {
+    
     private headers = {
         'X-Atlassian-Token': 'no-check',
         'Content-Type': 'application/json',
         'Authorization': `Basic ${btoa(`${Config.ADMIN_USERNAME}:${Config.ADMIN_PASSWORD}`)}`,
     }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const request = req.clone({ setHeaders: this.headers, withCredentials: true });
         console.log(request);
-        return next.handle(request).map((response) => {
-            console.log(response)
-            return response;
+        return next.handle(request).do((event: HttpEvent<any>) => {
+            console.log(event)
+        },(error: any) => {
+            console.log(error);
         });
     }
 }
