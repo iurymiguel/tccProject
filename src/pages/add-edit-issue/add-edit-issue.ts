99aa9c4064bd12adf7f3fc602646f007166c3cd6
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-10-13 20:34:37 -0300

    issue

--- src/pages/add-edit-issue/add-edit-issue.ts
+++ src/pages/add-edit-issue/add-edit-issue.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, Loading, ViewController } from 'ionic-angular';
 import { HttpServiceProvider } from '../../providers/http-service/http-service';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Utils } from '../../utils/utils';
-
-/**
- * Generated class for the AddEditIssuePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Config } from '../../config/config';
+import { ToastProvider } from '../../providers/toast/toast';
 
 @IonicPage()
 @Component({
@@ -20,53 +15,83 @@ export class AddEditIssuePage {
 
   public issue: any;
   public isEditing: boolean;
-  private formGroup: FormGroup;
-  public bluredInputs: any;
   public issueTypes: any[];
   public projectUsers: any[];
   public project: any;
+  public loading: Loading;
 
   constructor(
     public navCtrl: NavController,
+    public viewCtrl: ViewController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder,
+    public loadingCtrl: LoadingController,
+    public toast: ToastProvider,
     public http: HttpServiceProvider) {
 
-      this.formGroup = this.formBuilder.group({
-        summary : new FormControl('', [Validators.required, Validators.pattern(Utils.NO_SPACE_REGEX)]),
-        assignee: new FormControl('', [Validators.required, Validators.pattern(Utils.NO_SPACE_REGEX)]),
+    this.issue = this.navParams.get('issue');
+    this.projectUsers = this.navParams.get('projectUsers');
+    this.project = this.navParams.get('project');
+    this.issueTypes = this.navParams.get('issueTypes');
+    this.isEditing = !!this.issue;
+    if (!this.issue) {
+      this.issue = {
+        fields: {
+          project: {
+            id: this.project.id,
+          },
+          summary: '',
+          assignee: {
+            name: ''
+          },
+          issuetype: {
+            id: ''
+          },
+          labels: [
+            "todo"
+          ],
+          description: '',
+        }
+      };
+    }
+    
+  }
+
+
+  public ionViewWillEnter() {
+
+  }
+
+  public createIssue(){
+    this.loading = this.loadingCtrl.create({ content: 'Aguarde' });
+    this.loading.present();
+    console.log(this.issue);
+    this.http.post(`${Config.REST_API}/issue`, this.issue)
+      .then((result) => {
+        console.log(result);
+        this.loading.dismiss();
+        this.toast.show('Issue criada com sucesso').then(() => this.viewCtrl.dismiss());
+      })
+      .catch((error) => {
+        console.log(error);
+        this.loading.dismiss().then(() => this.toast.show('Erro na requisição.'));
       });
-      this.issue = this.navParams.get('issue');
-      this.projectUsers = this.navParams.get('projectUsers');
-      this.project = this.navParams.get('project');
-      this.issueTypes = this.navParams.get('issueTypes');
-      this.isEditing = !!this.issue;
-      this.setBluredInputState(false);
   }
 
+  public editIssue(){
 
-  public ionViewWillEnter(){
-  
   }
 
-
-  private insertIssue(){
-    if(this.formGroup.valid){
-
-    }else{
-      this.setBluredInputState(false);
-    }
-  }
-  
-  /**
-   * @description Seta true ou false se os inputs pederam o foco.
-   * @param state o estado do input.
-   */
-  private setBluredInputState(state: boolean) {
-    this.bluredInputs = {
-      isSummaryInputBlured: state,
-      isAssigneeInputBlured: state
-    }
+  public deleteIssue(){
+    this.loading = this.loadingCtrl.create({ content: 'Aguarde' });
+    this.loading.present();
+    this.http.delete(`${Config.REST_API}/issue/${this.issue.id}`)
+      .then((result) => {
+        this.loading.dismiss();
+        this.toast.show('Issue deletada com sucesso').then(() => this.viewCtrl.dismiss());
+      })
+      .catch((error) => {
+        this.loading.dismiss().then(() => this.toast.show('Erro na requisição.'));
+      });
   }
 
 
