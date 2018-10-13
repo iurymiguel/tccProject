c2f56eb54e580a0f2bf93f79ad8773c55facb30d
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-10-12 23:53:14 -0300

    modal issue

--- src/pages/add-edit-issue/add-edit-issue.ts
+++ src/pages/add-edit-issue/add-edit-issue.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Utils } from '../../utils/utils';
 
 /**
  * Generated class for the AddEditIssuePage page.
@@ -15,11 +18,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddEditIssuePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public issue: any;
+  public isEditing: boolean;
+  private formGroup: FormGroup;
+  public bluredInputs: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public http: HttpServiceProvider) {
+
+      this.formGroup = this.formBuilder.group({
+        summary : new FormControl('', [Validators.required, Validators.pattern(Utils.NO_SPACE_REGEX)]),
+        assignee: new FormControl('', [Validators.required, Validators.pattern(Utils.NO_SPACE_REGEX)]),
+      });
+      this.issue = this.navParams.get('issue');
+      this.isEditing = !!this.issue;
+      this.setBluredInputState(false);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddEditIssuePage');
+
+  public ionViewWillEnter(){
+  
   }
+
+
+  private insertIssue(){
+    if(this.formGroup.valid){
+
+    }else{
+      this.setBluredInputState(false);
+    }
+  }
+  
+  /**
+   * @description Seta true ou false se os inputs pederam o foco.
+   * @param state o estado do input.
+   */
+  private setBluredInputState(state: boolean) {
+    this.bluredInputs = {
+      isSummaryInputBlured: state,
+      isAssigneeInputBlured: state
+    }
+  }
+
 
 }
